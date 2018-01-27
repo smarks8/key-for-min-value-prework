5360d51735d2e5c36a02ce963b8458612c15a4a0
Stacey  Marks <staceyamarks@gmail.com>
2018-01-27 15:21:18 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,6 @@
 def key_for_min_value(name_hash)
   smallest_key = nil 
   smallest_value = nil 
-  
   name_hash.each do |key, value|
     if smallest_key == nil || value < smallest_key
       smallest_key = key 
@@ -13,3 +12,15 @@ def key_for_min_value(name_hash)
   end 
   smallest_key
 end
+
+# def key_for_min_value(hash)
+#   lowest_key = nil
+#   lowest_value = nil
+#   hash.each do |k, v|
+#     if lowest_value == nil || v < lowest_value
+#       lowest_value = v
+#       lowest_key = k
+#     end
+#   end
+#   lowest_key
+# end
