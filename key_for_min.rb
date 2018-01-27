dc56e47e072aa8bac155d9d0ab2b234f52dc5ccf
Stacey  Marks <staceyamarks@gmail.com>
2018-01-27 15:10:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   biggest_value = 0
-  key_for_min_value.each do |key, value|
+  name_hash.each do |key, value|
     if value > biggest_value
       biggest_value = value 
       biggest_key = key
